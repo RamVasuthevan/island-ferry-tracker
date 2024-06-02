b1a1e4704e5661d4a77aa9e790c4e3af221110d6
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2024-06-02 22:26:44 +0000

    Save webpage to output.html

--- src/json-schedule-generator/schedule_scraper.py
+++ src/json-schedule-generator/schedule_scraper.py
@@ -1,8 +1,6 @@
 import logging
-
 import requests
 from bs4 import BeautifulSoup
-
 
 class ScheduleScraper:
     SCHEDULE_URL: str = (
@@ -16,8 +14,17 @@ class ScheduleScraper:
         try:
             response = requests.get(ScheduleScraper.SCHEDULE_URL)
             response.raise_for_status()
+            ScheduleScraper.save_to_file(response.text, "../../output/schedules.html")
         except requests.HTTPError as error:
             ScheduleScraper.LOGGER.error(error)
             raise error
         ScheduleScraper.LOGGER.info("Successfully retrieved ferry schedules")
         return BeautifulSoup(response.text, "html.parser")
+
+    @staticmethod
+    def save_to_file(content: str, file_path: str) -> None:
+        ScheduleScraper.LOGGER.info(f"Saving webpage to {file_path}")
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(content)
+        ScheduleScraper.LOGGER.info(f"Webpage saved to {file_path}")
+
